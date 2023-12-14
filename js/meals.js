25413ac79bd4530d1c61a761653f229eb63e3923
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-15 00:36:31 +0600

    mealDB api load and  display

---
+++ js/meals.js
@@ -0,0 +1,37 @@
+const loadMeals = () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=fish")
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
+const displayMeals = (meals) => {
+  const mealContainer = document.getElementById("card-container");
+
+  meals.forEach((meal) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    // console.log(meals);
+    const img = meal.strMealThumb;
+    const title = meal.strMeal;
+    const descriptionsFull = meal.strInstructions;
+    const descriptions = descriptionsFull.slice(0, 120) + "...";
+    // console.log(img);
+
+    div.innerHTML = `
+     <div class="card h-100">
+                        <img src='${img}' class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${title}</h5>
+                            <p title='${descriptionsFull}' class="card-text">${descriptions}</p>
+                        </div>
+                    </div>
+    `;
+    mealContainer.appendChild(div);
+  });
+  //   console.log(meals[0].strMeal);
+};
+
+const searchMeal = () => {
+  console.log("clicked search btn");
+};
+
+loadMeals();
