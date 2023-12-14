707db052e8e44c73e7d3094d8f910fd55b9f0638
MD ABDULLAH <mdabdullah161036@gmail.com>
2023-12-14 21:36:10 +0600

    loadDetails

--- js/countries.js
+++ js/countries.js
@@ -14,19 +14,31 @@ const displayCountries = (countries) => {
     const Capital = country.capital;
     const continents = country.continents;
     const population = country.population;
-
-    console.log(name);
+    // dynamic details transfer
+    const code = country.cca2;
+    // console.log(code);
+    // console.log(name);
     div.innerHTML = `
                 <img src=${flag}>
                  <h1>Name: ${name}</h1>
                 <h3>Capital: ${Capital}</h3>
                 <h3>Continents: ${continents}</h3>
-                <p>Population: ${population} </p>
-                
+                <p>Population: ${population}</p>
+                <button onclick="loadDetails('${code}')" type="">Details</button>
+
          `;
     countriesContainer.appendChild(div);
-    console.log(country);
+    // console.log(country);
   });
 };
+const loadDetails = (code) => {
+  const url = `https://restcountries.com/v3.1/alpha/${code}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayDetails(data[0]));
+};
 
+const displayDetails = (details) => {
+  console.log(details.name.common);
+};
 loadCountries();
